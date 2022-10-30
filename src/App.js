6278859aada5551c1b4d7e48c769f97a2e7ae106
Alfiano Deni Kusuma <dedekkecil46@gmail.com>
2022-10-30 15:43:46 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Title from './Header/Title';
+import Tabel from './Body/Tabel';
+import Footer from './footer/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+      <Tabel/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
